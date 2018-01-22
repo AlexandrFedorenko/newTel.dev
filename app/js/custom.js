3225dac7a13e5a32793755833d5ed50bcaaa8d96
Alexandr <alexandrfedorenkoooo@gmail.com>
2018-01-22 17:01:28 +0200

    finish

--- app/js/custom.js
+++ app/js/custom.js
@@ -110,6 +110,42 @@ $(document).ready(function() {
             }
         }
     });
+
+    $('.popup-gallery2').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function(item) {
+                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            }
+        }
+    });
+
+    $('.popup-gallery3').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function(item) {
+                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            }
+        }
+    });
 });
 
 
@@ -118,3 +154,19 @@ function setClassActiv() {
     $(this).toggleClass('filter-btn--active');
 }
 $('.filter-btn').click(setClassActiv);
+
+
+//scroll in page FAQ
+
+$(".faq_item a").on("click", function(e) {
+
+    e.preventDefault();
+
+    var currentBlock = $(this).attr("href"),
+        currentBlockOffset = $(currentBlock).offset().top;
+
+    $("html, body").animate({
+        scrollTop: currentBlockOffset - 20
+    }, 500);
+
+});
